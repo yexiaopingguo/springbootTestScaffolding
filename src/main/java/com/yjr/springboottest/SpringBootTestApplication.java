c4ec7aee98c0905f9a4bfed81dbb6b8e4e2ec29f
yexiaopingguo <1056527538@qq.com>
2025-05-05 15:33:22 +0800

    测试

--- src/main/java/com/yjr/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/yjr/springboottest/SpringBootTestApplication.java
@@ -4,10 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-
 @SpringBootApplication
 @MapperScan("com.yjr.springboottest.mapper")
 public class SpringBootTestApplication {
